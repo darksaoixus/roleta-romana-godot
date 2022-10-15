ab31587d2f95e3b45d02e1e316a1a59f5621b304
baianosaoik123 <edubru06@gmail.com>
2022-10-15 14:20:51 -0300

    C# project and list

---
+++ scripts/LElement.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class LElement
+{
+    private String _name;
+
+    public String Name { get { return _name; } }
+    public LElement Next;
+
+    public LElement(String name) {
+        _name = name;
+    }
+}
---
+++ scripts/List.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public class List : Reference
+{
+    private LElement _head;
+    private int _size;
+
+    [Signal]
+    private delegate void RemovedElement(String name);
+
+    public void Append(String name) {
+        LElement elementToAppend = new LElement(name);
+        if (_head == null)
+            _head = elementToAppend;
+        else
+        {
+            LElement el = _head;
+            while (el.Next != _head) el = el.Next;
+            el.Next = elementToAppend;
+            elementToAppend.Next = _head;
+        }
+        _size++;
+    }
+
+    public void Remove(int n) {
+        while (_size != 1)
+        {
+            LElement el = _head;
+            LElement previous = null;
+            for (int i = 0; i < n; i++)
+            {
+                previous = el;
+                el = el.Next;
+            }
+            previous.Next = el.Next;
+            EmitSignal(nameof(RemovedElement), el.Name);
+        }
+    }
+}
